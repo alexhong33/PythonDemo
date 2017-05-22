ed3bd628467b5ed64f7f8c9862d36ac9bec7fcea
alexhong33 <alexhongf@163.com>
2017-05-22 15:13:03 +0800

    book exercise 2, 3, 4, 5

---
+++ PythonDemo/Day01/01print.py
@@ -0,0 +1,46 @@
+#book ex1-3
+
+
+print ('Hello World')
+print ("Hello Again")
+print ('I like typing this.')
+print ('This is fun.')
+print ('Yay! Printing.')
+print ("I'd much rather you 'not'.")
+print ('I "said" do not touch this.')
+print ('你好!')
+#print ('#1')
+
+
+# A comment, this is so you can read your program later.
+# Anything after this # is ignored by python
+
+print("I could have code like this.") # and the comment after is ignored
+
+# You can also use a comment to "disable" or comment out a piece of code:
+# print "This won't run."
+
+print("This will run.")
+
+
+
+# this is the first Comment
+spam = 1 # and this is the second Comment
+# ... and now a third!
+text = " # This is not a comment because it's inside quotes."   
+
+
+print (2 + 2)
+print (50 - 5 * 6)
+print ((50 - 5 * 6) / 4)
+
+print (8/5) #division always returns a floating point number
+print (8//5) #获得整数
+print (8%5) #获得余数
+
+print (5 * 3 + 2)
+print (5 ** 2) #5 squared
+print (2 ** 8)
+
+print (2.5 * 4 / 5.0)
+print(7.0/2) #python完全支持浮点数, 不同类型的操作数混在一起时, 操作符会把整型转化为浮点型
---
+++ PythonDemo/Day01/book05.py
@@ -0,0 +1,60 @@
+'''
+Created on 2017年5月22日
+
+@author: Alex
+
+总结:
+本节 学习使用Python 格式化字符
+'''
+
+
+'''
+%s    字符串 (采用str()的显示)
+
+%r    字符串 (采用repr()的显示)
+
+%c    单个字符
+
+%b    二进制整数
+
+%d    十进制整数
+
+%i    十进制整数
+
+%o    八进制整数
+
+%x    十六进制整数
+
+%e    指数 (基底写为e)
+
+%E    指数 (基底写为E)
+
+%f    浮点数
+
+%F    浮点数，与上相同
+
+%g    指数(e)或浮点数 (根据显示长度)
+
+%G    指数(E)或浮点数 (根据显示长度)
+          
+'''
+
+my_name = 'Zed A. Shaw'
+my_age = 25 # not a lie
+my_height = 171 # cm
+my_weight = 140 # kg
+my_eyes = 'Brown'
+my_teeth = 'White'
+my_hair = 'Black'
+
+print("Let's talk about %s." %my_name)
+print("He's %d cm tall." %my_height)
+print("He's %d kg heavy." %my_weight)
+print("Actually that's not too heavy.")
+print("He's got %s eyes and %s hair." %(my_eyes, my_hair))
+print("His teeth are usually %s depending on the coffee." %my_teeth)
+
+
+# this line is tricky, try to get it exactly right
+print("If I add %d, %d, and %d I get %d." %(
+    my_age, my_height, my_weight, my_age+my_height+my_weight))
