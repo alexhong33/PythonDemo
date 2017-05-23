fb3d8d8604f1f8ad9c74a1a9532cb0b7393d090a
alexhong33 <alexhongf@163.com>
2017-05-23 17:23:16 +0800

    Day02, add demo1 urllib

---
+++ PythonDemo/Day02/demo01.py
@@ -0,0 +1,15 @@
+'''
+Created on 2017年5月23日
+
+@author: Admin_lgf
+'''
+
+from urllib import request
+
+with request.urlopen('https://api.douban.com/v2/book/2129650') as f:
+    data = f.read()
+    print('Status:', f.status, f.reason)
+    for k, v in f.getheaders():
+        print('%s: %s' %(k, v))
+    print('Data:', data.decode('utf-8'))
+
