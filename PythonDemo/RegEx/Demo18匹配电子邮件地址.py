06410854ea1e835fc81d9a7f2d80a874ed989e76
alexhong33 <alexhongf@163.com>
2017-06-06 17:17:10 +0800

    RegEx

--- PythonDemo/RegEx/Demo18匹配电子邮件地址.py
+++ PythonDemo/RegEx/Demo18匹配电子邮件地址.py
@@ -9,8 +9,8 @@ import re
 
 pattern = "\w+([.+-]\w+)*@\w+([.-]\w+)*\.\w+([.-]\w+)*"
 
-#string = "<a href='http://www.baidu.com'>百度首页</a><br><a href='mailto:c_e+o@iqi-anyue.com.cn>电子邮箱地址</a>"
-string = "<a href='http://www.baidu.com'>百度首页</a><br><a href='alexhongf@163.com>电子邮箱地址</a>"
+string = "<a href='http://www.baidu.com'>百度首页</a><br><a href='mailto:c_e+o@iqi-anyue.com.cn>电子邮箱地址</a>"
+
 result = re.search(pattern, string)
 print(result)
 
